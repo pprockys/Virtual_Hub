1c84d9e4cb1ddf63de4f527d8b7ea42c90c9bf68
Aman kumar singh <maskmanlucifer@gmail.com>
2020-05-31 11:37:38 +0530

    change

--- vh.js
+++ vh.js
@@ -1,9 +1,13 @@
 let prelink="https://codeforces.com/contest/";
 let v1;
+document.getElementById("customcontestnumber").onkeyup="storecontestnumber()";
+
 function storecontestnumber()
 {
     v1=document.getElementById("customcontestnumber").value;
 }
+
+document.getElementById("go").onclick="openvirtualpage()";
 function openvirtualpage()
 {
     if(v1<1 || v1>1359)
