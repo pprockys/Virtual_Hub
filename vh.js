657b5058fd9e2d739fcdfe8ec2cd97447afb7faf
Ayush avasthi <avasthiayush22@gmail.com>
2020-06-01 14:39:20 +0530

    add css

--- vh.js
+++ vh.js
@@ -1,5 +1,4 @@
 let v;
-
 let prelink="https://codeforces.com/contest/";
 document.addEventListener("keyup",contestno);
 
@@ -7,22 +6,17 @@ function contestno()
 {
     v=document.getElementById("customcontestnumber").value;
 }
-
-document.getElementById("butt").addEventListener("click",opencontest);
-
-function opencontest()
+if(v<1 || v>1359)
 {
-   if(v>=1 && v<=1359)
-   {
-    let fulllink=prelink+v;
-    window.open(fulllink);
-   }
-   else if(v==null)
-   {
-      document.getElementById("para").innerHTML="Sorry! we can not bring this page to you.";
-   }
-   else
-   {
-      document.getElementById("para").innerHTML="Sorry! we can not bring this page to you.";
-   }
+    window.alert("Sorry this doesn't exist");
+}
+else
+{
+
+  document.getElementById("butt").addEventListener("click",opencontest);
+  function opencontest()
+  {
+   let fulllink=prelink+v;
+   window.open(fulllink);
+  }
 }
